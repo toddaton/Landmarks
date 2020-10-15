53a9733fafed3d72e19412bda3ad63177ba868b6
Todd Aton <toddaton@hotmail.com>
2020-10-15 08:31:54 -0700

    New project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Todd on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
